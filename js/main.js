0e572784996bf4199c8582e7322a6bb2b5c48671
Katie Miller <katie@codemiller.com>
2012-12-01 13:28:11 +1000

    Added jQuery to do call to action show/hide and cross fades

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-
+$(window).load(function() {
+    var selected = undefined;
+    $("div#calls-to-action a").bind("click",function(){
+        if ($(this).attr("href")==selected) {
+             $("div#call-to-action-detail").slideToggle();
+        } else {
+            selected = $(this).attr("href");
+            $("div#call-to-action-detail div.cta-detail-content").hide();
+            if($("div#call-to-action-detail").is(":visible")) {
+                $("div#call-to-action-detail div" + selected).fadeIn();
+            } else {
+                $("div#call-to-action-detail").slideToggle("fast", function() {
+                    $("div#call-to-action-detail div" + selected).fadeIn();
+                });
+            }
+        }
+        return false;
+    });
+});
